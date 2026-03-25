fec95121a418b8b6b03aa1f13c6913c57f615c3a
santiago123-dex <santiagofajardomorales17@gmail.com>
2026-03-25 06:48:49 -0500

    feat(auth): add registration screen

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "../../src/features/auth/components/register/register"
+
+export default function Register() {
+  return (
+    <div>
+      <RegisterForm></RegisterForm>
+    </div>
+  )
+}
